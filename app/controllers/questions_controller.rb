508be8a02d20b8718ac9016d27bea94ef38854f3
Anne Ly <anne.ly59@gmail.com>
2022-11-14 13:07:06 +0100

    rails stupid finished

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,13 @@
+class QuestionsController < ApplicationController
+  def answer
+    @answer = ''
+    @question = params[:question]
+    if @question.include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif @question.include?('I am going to work')
+      @answer = 'Great'
+    else
+      @answer = 'I don\'t care, get dressed and go to work!'
+    end
+  end
+end
